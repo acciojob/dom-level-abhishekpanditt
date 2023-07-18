2f315fbefc3197b5e44cbe742201b03f1de9c609
abhishekpanditt <123313123+abhishekpanditt@users.noreply.github.com>
2023-07-18 13:41:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//your JS code here. If required.
+const lists = document.querySelectorAll("li");
+let levCount = 0;
+for(let i = 0; i<lists.length; i++){
+	levCount++;
+	if(lists[i].id==="level"){
+		break;
+	}
+}
+alert(`The level of the element is: ${levCount}`);
